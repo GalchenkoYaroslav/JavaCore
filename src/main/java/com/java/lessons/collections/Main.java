c49cfe2575334874b4fe448c1bbe6258929dfac4
GalchenkoYaroslav <yarosgalchenko@gmail.com>
2016-12-13 20:08:50 +0200

    second commit

--- src/main/java/com/java/lessons/collections/Main.java
+++ src/main/java/com/java/lessons/collections/Main.java
@@ -1,8 +1,5 @@
 package com.java.lessons.collections;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by Yaroslav on 07.12.2016.
@@ -10,9 +7,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
         String s1 = "AAA";
         String s3 = new String("AAA").intern();
-//        System.out.println(s1==s3);
+
     }
 }
